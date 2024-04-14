8a75252aafbf801516f04d154ce41df3aa89a609
RA4Z <rraz639@gmail.com>
2024-04-14 19:14:49 -0300

    Estilizando botão para navegar até o catálogo

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,13 +1,20 @@
 import styles from './Homepage.module.scss'
 import Carrossel from 'components/Carrossel'
+import seta from 'assets/seta-para-cima-direita.svg'
+import { useNavigate } from 'react-router-dom'
 
 export default function Homepage() {
+    const navigate = useNavigate()
     return (
         <div className={styles.corpo}>
             <div className={styles.container}>
                 <div className={styles.titulo}>
                     <li>Criando idéias</li>
                     <li className={styles.sonhos}>e estampando </li>
+                    <button onClick={() => navigate('/Catalogo')}>
+                        <label>Vamos lá!</label>
+                        <img src={seta} alt='Seta para cima' />
+                    </button>
                 </div>
                 <div className={styles.carousel}>
                     <Carrossel />
