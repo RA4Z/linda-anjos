fc85bb9e30df2a057a3126d0be0d6da15af1730b
RA4Z <rraz639@gmail.com>
2024-04-24 20:54:56 -0300

    Adicionando telefone correto

--- src/types/database.ts
+++ src/types/database.ts
@@ -1,6 +1,6 @@
 const admMail = 'linda-anjos@tech.com'
 const tablePath = 'Linda_Anjos'
 const imagesPath = 'Linda_Anjos/Products'
-const phoneNumber = '5547912345678'
+const phoneNumber = '5513997174097'
 
 export {tablePath, imagesPath, admMail, phoneNumber}
